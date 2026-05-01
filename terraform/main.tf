50dceae3d2005477c6b3e1d9dc04abf22ddd7cbe
ss23-bit <ss23lonewolf@gmail.com>
2026-05-01 13:49:27 +0700

    feat: add CI/CD pipeline on GitHub Actions using ECR image, and Infrastructure as code using Terraform to provision for AWS

---
+++ terraform/main.tf
@@ -0,0 +1,49 @@
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+resource "aws_instance" "devops_server" {
+  ami           = "ami-0a56f8447277affd8"
+  instance_type = "t3.micro"
+  key_name      = var.key_name
+
+  security_groups = [aws_security_group.devops_sg.name]
+
+  tags = {
+    Name = "devops-terraform-server"
+  }
+
+  user_data = <<-EOF
+              #!/bin/bash
+              apt update -y
+              apt install docker.io -y
+              systemctl start docker
+              systemctl enable docker
+              usermod -aG docker ubuntu
+              EOF
+}
+
+resource "aws_security_group" "devops_sg" {
+  name = "devops-sg"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
